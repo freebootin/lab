06e8679d5b7f60d151d931ec0ef75fb3f230eb4d
freebootin <patrick@freebootin.com>
2022-05-31 19:09:38 -0700

    added comments

--- examples-c/tokinize-a-string.c
+++ examples-c/tokinize-a-string.c
@@ -9,6 +9,13 @@ int main(int argc, char *argv[])
   char *s;
 
 
+  /* char *strtok(char *s1, const char *s2)
+   * s1 is the string you want to parse, s2 is the string of delimiters you want
+   * to parse with. for whitespace you can use " \t\n". 
+   *
+   * After the first call to strtok, you need to change s1 to NULL if you want to
+   * keep parsing the same string. When strtok returns NULL, the parsing is done.
+   */
   for (s = my_str; (s = strtok(s, " ")) != NULL; s = NULL) {
     printf("%s\n", s);
   }
